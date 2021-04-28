baa419ea19bd7b7c0cbcaf459e64eb440da848f9
letmetest1822-sami <letmetest1822@gmail.com>
2021-04-28 14:31:59 +0300

    Spring Container

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,8 +1,6 @@
 import interfaces.Mentor;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import services.FullTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
@@ -20,5 +18,9 @@ public class CybertekApp {
         Mentor mentor1 = container.getBean("fullTimeMentor", Mentor.class); // no need to down cast in this way
 
         mentor1.createAccount();
+
+        Mentor mentor2 = (Mentor) container.getBean("partTimeMentor");
+
+        mentor2.createAccount();
     }
 }
