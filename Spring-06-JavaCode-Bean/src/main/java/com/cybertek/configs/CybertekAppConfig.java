c8c1076331fd7d5bc2f0224f67fe8246d7c20192
SamKar <letmedevelopsam@gmail.com>
2021-05-03 03:35:02 +0300

    06 - JavaCode - Bean

---
+++ Spring-06-JavaCode-Bean/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -0,0 +1,44 @@
+package com.cybertek.configs;
+
+import com.cybertek.services.Java;
+import com.cybertek.services.Selenium;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cybertek")
+public class CybertekAppConfig {
+
+    /*
+    First thing we do is add annotations
+@Configuration
+@ComponentScan
+then tell spring the package where to look for beans
+@ComponentScan("com.cybertek")
+
+Then it will go and look for @Component Annotations in the classes
+If there is no annotation you will receive :
+Exception in thread "main" org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named 'java' available
+Instead of adding all classes @Component annotation we can create methods with @Bean annotations which will provide us Beans
+
+
+Finally, go to CybertekApp:
+ ApplicationContext container = new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+ giving the configuration Java class is important                    //(CybertekAppConfig.class);
+
+ old one was: but now no xml needed
+ ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+     */
+
+    @Bean
+    public Java java(){
+        return new Java();
+
+    }
+    @Bean
+    public Selenium selenium(){
+        return new Selenium();
+    }
+}
