a24863e62aa42afc75f9da7236875276cd6efeed
SamKar <letmedevelopsam@gmail.com>
2021-05-04 14:49:05 +0300

    07 - SpringBoot - CarpetAssignment

---
+++ Spring-07-CarpetAssignment/src/main/java/com/cybertek/Spring07CarpetAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07CarpetAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring07CarpetAssignmentApplication.class, args);
+    }
+
+}
