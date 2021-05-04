aa87835521177288587e9550155e88f5da7dd80e
SamKar <letmedevelopsam@gmail.com>
2021-05-04 15:44:38 +0300

    07 - SpringBoot - Demo

--- Spring-07-CarpetAssignment/src/main/java/com/cybertek/Spring07CarpetAssignmentApplication.java
+++ Spring-07-CarpetAssignment/src/main/java/com/cybertek/Spring07CarpetAssignmentApplication.java
@@ -1,13 +1,22 @@
 package com.cybertek;
 
+import com.cybertek.calculator.Calculator;
+import com.cybertek.enums.City;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring07CarpetAssignmentApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring07CarpetAssignmentApplication.class, args);
+    public static void main(String[] args) throws Exception {
+
+        // just add ApplicationContext container =
+        ApplicationContext container = SpringApplication.run(Spring07CarpetAssignmentApplication.class, args);
+        Calculator calculator = container.getBean("calculator", Calculator.class);
+
+        System.out.println(calculator.getTotalCarpetCost(City.ARLINGTON));
+
     }
 
 }
---
+++ Spring-07-CarpetAssignment/src/main/java/com/cybertek/calculator/Calculator.java
@@ -0,0 +1,35 @@
+package com.cybertek.calculator;
+
+import com.cybertek.enums.City;
+import com.cybertek.interfaces.carpetPrices.Carpet;
+import com.cybertek.interfaces.floorTypes.Floor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class Calculator {
+
+    @Autowired
+    @Qualifier("carpetVA")
+    private Carpet carpet;
+
+    @Autowired
+    @Qualifier("kitchen")
+    private Floor floor;
+
+    public String getTotalCarpetCost(City city) throws Exception {
+
+        BigDecimal cost = carpet.getSqFtPrice(city).multiply(floor.getArea());
+
+        if(cost.compareTo(BigDecimal.ZERO)==0){
+            throw  new Exception("This City doeas NOT exist");
+        }
+
+        return "Total Cost for the Carpet : " + cost;
+    }
+
+
+}
