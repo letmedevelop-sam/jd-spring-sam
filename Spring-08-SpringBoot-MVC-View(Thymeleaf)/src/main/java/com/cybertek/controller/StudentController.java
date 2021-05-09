b4667eb07aca37f46978c3498e1e46a441c97463
SamKar <letmedevelopsam@gmail.com>
2021-05-10 02:23:19 +0300

    08 - SpringBoot - MVC - View - Thymeleaf Text

--- Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +45,10 @@ public class StudentController {
         LocalDate birthday = LocalDate.now().minusYears(43);
 
         model.addAttribute("birthday", birthday);
+
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
 
 
