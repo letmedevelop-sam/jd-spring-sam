88af3f81778e87dad2fdaedcdca367f53210a87c
SamKar <letmedevelopsam@gmail.com>
2021-05-09 14:36:34 +0300

    08 - SpringBoot - MVC - Controller - Get mapping

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,26 +21,42 @@ public class HomeController {
 
     //after income request, a template will return
 
-    @RequestMapping(method = RequestMethod.GET, value ="/ozzy")
+    @RequestMapping(method = RequestMethod.GET, value ="/sam")
     public  String getRequestMapping2(){
-        return  "home";
+        return  "home-sam";
     }
 //method name DOES NOT MATTER. it is looking for the end point in annotation
 //we use GET and POST for UI. For REST we will use more...
 
-    @RequestMapping(method = RequestMethod.POST, value ="/ozzy")
+    @RequestMapping(method = RequestMethod.POST, value ="/cybertek")
     public  String getRequestMapping3(){
-        return  "home";
+        return  "home-cybertek";
     }
 
     @GetMapping("/spring")
     public String getMappingEx(){
-        return "home";
+        return "spring";
     }
 
-    @GetMapping("/spring")
-    public String getMappingEx2(){
-        return "home";
+//    @GetMapping("/spring")
+//    public String getMappingEx2(){
+//        return "home";
+//    }
+
+    //In API we will use this structure a lot we will retrieve endpoint for URI
+    @GetMapping("/home/{department}")         //using {  ccc }  will let us use any name as parameter
+    public String pathVariableEx(@PathVariable("department") String deptName){
+        System.out.println("Name given in the URI is  : " + deptName);
+        return "home-variable";
     }
+
+    //This code will bring some data from UI to java
+    @GetMapping("/home/{department}/{section}")         //using {  ccc }  will let us use any name as parameter
+    public String pathVariableEx2(@PathVariable("department") String deptName, @PathVariable("section") String sectName){
+        System.out.println("The department name given in the URI is  : " + deptName);
+        System.out.println("The  section  name  given in the URI is  : " + sectName);
+        return "home-variable-sub";
+    }
+
 
 }
