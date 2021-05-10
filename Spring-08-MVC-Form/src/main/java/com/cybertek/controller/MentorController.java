4370d53e2a0456eb9d435e6c065efefb0db98c8c
SamKar <letmedevelopsam@gmail.com>
2021-05-11 00:04:49 +0300

    08 - SpringBoot - MVC - Form - Typo fix

--- Spring-08-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -23,7 +23,7 @@ public class MentorController {
         List<String > batchList = Arrays.asList("B7", "B8","B9","B10","B11","B12","E1", "E2", "E3", "E4");
         model.addAttribute("batchList",batchList);
 
-        return "mentor/mentor-register";
+        return "/mentor/mentor-register";
     }
 
 
