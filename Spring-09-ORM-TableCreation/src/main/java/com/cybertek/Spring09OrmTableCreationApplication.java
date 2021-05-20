37306592b3d34ed74a01e7cce9f4afc3b4303c89
SamKar <letmedevelopsam@gmail.com>
2021-05-20 13:30:42 +0300

    Spring-09-ORM-TableCreation

---
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/Spring09OrmTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09OrmTableCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09OrmTableCreationApplication.class, args);
+    }
+
+}
