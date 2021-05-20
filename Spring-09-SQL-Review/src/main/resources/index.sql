bef2fc4ffc015b11dd2af134ef390135f879a05e
SamKar <letmedevelopsam@gmail.com>
2021-05-20 11:41:06 +0300

    towns

--- Spring-09-SQL-Review/src/main/resources/index.sql
+++ Spring-09-SQL-Review/src/main/resources/index.sql
@@ -1,4 +1,21 @@
 EXPLAIN ANALYZE
+
+CREATE TABLE Towns (
+                    id SERIAL UNIQUE NOT NULL,
+                    code VARCHAR (10) NOT NULL, --not unique
+                    article TEXT,
+                    name TEXT NOT NULL  --not unique
+);
+
+insert into  towns (code, article, name) SELECT
+    left(md5(i::text), 10),
+    md5(random()::text),
+    md5(random()::text)
+from generate_series(1,1000000) s(i);
+
+select count(*)
+from towns;
+
 select *
 from towns
 where name='bd7555a25ccb6849938cc2f7e8c0d03f';
