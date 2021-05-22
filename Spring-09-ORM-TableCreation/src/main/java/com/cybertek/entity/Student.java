88079efd1c4441c6caad31231536ef59decb7fb3
SamKar <letmedevelopsam@gmail.com>
2021-05-23 00:04:05 +0300

    Spring - 09 - ORM - Table Creation Column names added

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -9,8 +9,11 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //as we use camelCase  structure in table we will see _ between words
+    @Column(name = "studentFirstName")
     private String firstName;  //first_name
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentFirstEmail")
     private String email;
 
 
