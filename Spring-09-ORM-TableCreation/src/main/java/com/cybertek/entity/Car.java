b9828c78b729934f611a6d5f8a2c1ce8d06118b1
SamKar <letmedevelopsam@gmail.com>
2021-05-23 00:50:00 +0300

    Spring - 09 - ORM - Table Creation Repositories added

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,16 +1,29 @@
 package com.cybertek.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity          // I want a Car table. It will have id, make and model columns. Adding annotation started relational mapping
 @Table(name = "cars")
+@NoArgsConstructor// add lombok to create a n object  / constructor to add something to our table
+//@AllArgsConstructor  //we removed @AllArgsConstructor  bc of ID issue
+@Getter
+@Setter
 public class Car {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //this will generate id for us
     private int id;
 
     private String make;
     private String model;
 
-
+    public Car(String make, String model) {  //as id will be generated automat,cally we will cretae another constructor without ID
+        this.make = make;
+        this.model = model;
+    }
 }
