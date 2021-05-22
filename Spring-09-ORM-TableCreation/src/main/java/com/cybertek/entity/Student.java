95ec9013a57e3fb1e066bc4e3b5b0c597c46dace
SamKar <letmedevelopsam@gmail.com>
2021-05-23 00:26:37 +0300

    Spring - 09 - ORM - Table Creation Enum GENDER  added

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,6 +1,9 @@
 package com.cybertek.entity;
 
+import com.cybertek.enums.Gender;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -15,6 +18,24 @@ public class Student {
     private String lastName;
     @Column(name = "studentFirstEmail")
     private String email;
+
+    @Transient
+    private String city;
+
+    @Temporal(TemporalType.DATE)
+ //   @Column(columnDefinition = "DATE")          //Java 8
+    private Date birthDate;
+
+    @Temporal(TemporalType.TIME)
+//    @Column(columnDefinition = "TIME")          //Java 8
+    private Date birthTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(columnDefinition = "TIMESTAMP")         //Java 8
+    private Date getBirthDateTime;
+
+    @Enumerated (EnumType.STRING)  //it comes as  @Enumerated (EnumType.ORDINAL) //change it to STRING
+    private Gender gender;  //normally it automatically comes as integer to DB as 0 or 1
 
 
 
