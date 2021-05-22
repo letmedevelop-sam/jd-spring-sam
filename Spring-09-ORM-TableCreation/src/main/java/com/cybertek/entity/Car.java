5ba52b73f3353709e5ece3c9643f8221ed405c06
SamKar <letmedevelopsam@gmail.com>
2021-05-22 19:35:07 +0300

    Spring - 09 - ORM - Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -3,11 +3,12 @@ package com.cybertek.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity          // I want a Car table. It will have id, make and model columns
+@Entity          // I want a Car table. It will have id, make and model columns. Adding annotation started relational mapping
 public class Car {
 
     @Id
     private int id;
+
     private String make;
     private String model;
 
