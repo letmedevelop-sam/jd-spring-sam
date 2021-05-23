5b4ad55c4e92e79cbd7a21d0849b124ee6aafcbf
SamKar <letmedevelopsam@gmail.com>
2021-05-23 16:52:14 +0300

    Spring - 09 - ORM - OneToMany created

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Employee.java
@@ -0,0 +1,51 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor  //As we created param constructor we also need to create no args constructor
+public class Employee extends BaseEntity{
+
+    //id will be inherited from BaseEntity
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+//    private String department;        //This column will come from JOIN
+
+                //cascade ALL will tell Hibernate when you do something in one table, go and do the same in the other table
+    @ManyToOne(cascade = CascadeType.ALL) //For taking not all of them use one by one {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST}
+    @JoinColumn(name = "departmentID")
+    private Department department; //it is complaining and we need to add @OneToOne annotation to describe how tables will join (one Emp will work in One Dept
+
+    @Enumerated (EnumType.STRING)
+    private Gender gender;
+    private int salary;
+//    private int regionId;         //This column will come from JOIN
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY) //fetch = FetchType.LAZY do when needed  or else fetch = FetchType.EAGER
+    @JoinColumn(name = "regionId")
+    private Region region;
+
+    //create constructor
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.hireDate = hireDate;
+        this.gender = gender;
+        this.salary = salary;
+    }
+}
---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/cybertek/entity/Region.java
@@ -0,0 +1,29 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "regions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Region extends BaseEntity{
+
+ //   private int regionId;     //Id will be inherited
+    private  String region;
+    private String country;
+
+    @OneToOne(mappedBy = "region")           //means DO NOT create a Foreign key  column in my table
+    private Employee employee;
+
+    public Region(String region, String country) {
+        this.region = region;
+        this.country = country;
+    }
+}
