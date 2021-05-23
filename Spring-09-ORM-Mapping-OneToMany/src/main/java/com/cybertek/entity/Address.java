764f8fd052f1eda208dfce1194562c7cd0632495
SamKar <letmedevelopsam@gmail.com>
2021-05-23 22:22:35 +0300

    Spring - 09 - ORM - OneTMany created

--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Address.java
@@ -2,10 +2,17 @@ package com.cybertek.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Address {
 
     @Id
     private Long id;
+
+    private String street;
+    private String zipCode;
+
+    @ManyToOne    //mapped by can not be used in ManyToOne
+    private Person person;
 }
--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -1,6 +1,7 @@
 package com.cybertek.entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Person {
@@ -12,7 +13,15 @@ public class Person {
     private String firstName;
     private String lastName;
 
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;  //'One To Many' attribute type should be a container, so we need to use collection
+
+    /*
+    We can do it in a different way as well =>
+
     @OneToMany
-    private Address address;
+    @JoinColumn(name = "person_id")
+    private List<Address> addresses;
+    */
 
 }
