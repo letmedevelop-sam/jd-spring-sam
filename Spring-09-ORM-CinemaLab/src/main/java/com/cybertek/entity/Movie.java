a0d614c89a61e592241b1de7a52d420c6a073464
SamKar <letmedevelopsam@gmail.com>
2021-05-24 20:48:36 +0300

    Spring - 09 - ORM - Cinema Lab App Movie updated enums added

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
@@ -1,10 +1,15 @@
 package com.cybertek.entity;
 
+import com.cybertek.enums.MovieState;
+import com.cybertek.enums.MovieType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -12,7 +17,21 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 public class Movie extends  BaseEntity{
 
+private  String name;
 
+@Column(columnDefinition = "DATE")
+private LocalDate releaseDate;
+
+private Integer duration;
+
+@Column(columnDefinition = "text")
+private String summary;
+
+private MovieType type;
+
+private MovieState state;
+
+private BigDecimal price;
 
 
 }
