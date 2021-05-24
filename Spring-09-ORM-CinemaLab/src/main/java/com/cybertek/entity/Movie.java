cccf62092ea970f050dd2062e0d601b5b2a7d8c9
SamKar <letmedevelopsam@gmail.com>
2021-05-24 21:18:20 +0300

    Spring - 09 - ORM - Cinema Lab App Genre added

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
@@ -6,10 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,21 +18,42 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Movie extends  BaseEntity{
 
-private  String name;
+    private  String name;
 
-@Column(columnDefinition = "DATE")
-private LocalDate releaseDate;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
 
-private Integer duration;
+    private Integer duration;
 
-@Column(columnDefinition = "text")
-private String summary;
+    @Column(columnDefinition = "text")
+    private String summary;
 
-private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
 
-private MovieState state;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
 
-private BigDecimal price;
+    private BigDecimal price;
 
 
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+
+    private List<Genre> genreList = new ArrayList<>();
+
+
+
+    //IF YOU WORK WITH data.sql THEN YOU DONT NEED TO CREATE CONSTRUCTORS
+    public Movie(String name, LocalDate releaseDate, Integer duration, String summary, MovieType type, MovieState state, BigDecimal price) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.summary = summary;
+        this.type = type;
+        this.state = state;
+        this.price = price;
+    }
 }
--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/enums/MovieState.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/enums/MovieState.java
@@ -2,5 +2,5 @@ package com.cybertek.enums;
 
 public enum MovieState {
 
-    ACTIVE, DRAFT
+    ACTIVE, DRAFT, SUSPENDED
 }
