1ae4569f75fea53b782085352c9664b3279b7632
SamKar <letmedevelopsam@gmail.com>
2021-05-24 22:42:53 +0300

    Spring - 09 - ORM - Cinema Lab App Cinema Movie Cinema added

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -0,0 +1,17 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cinema extends BaseEntity{
+
+    private String name;
+    private String sponsoredName;
+}
