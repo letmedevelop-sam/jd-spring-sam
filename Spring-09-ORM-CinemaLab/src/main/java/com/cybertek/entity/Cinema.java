e01f03978765961d56ef38c23fb18849c29301bd
SamKar <letmedevelopsam@gmail.com>
2021-05-24 23:03:09 +0300

    Spring - 09 - ORM - Cinema Lab App Location - Ticket added

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -14,4 +16,11 @@ public class Cinema extends BaseEntity{
 
     private String name;
     private String sponsoredName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+    public Cinema(String name) {
+        this.name = name;
+    }
 }
---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Ticket.java
@@ -0,0 +1,39 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ticket extends BaseEntity{
+
+    @Column(columnDefinition = "seat_number")
+    private Integer seatNumber;
+
+    @Column(columnDefinition = "row_number")
+    private Integer rowNumber;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_cinema_id")
+    private MovieCinema movieCinema;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_account_id")
+    private User user;
+
+    public Ticket(Integer seatNumber, Integer rowNumber, LocalDateTime localDateTime) {
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.localDateTime = localDateTime;
+    }
+}
