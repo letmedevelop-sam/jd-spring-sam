eb5f04ae8d439f2f245b42eae975b4afbc7a5ffd
SamKar <letmedevelopsam@gmail.com>
2021-05-30 14:18:22 +0300

    Spring - 09 - ORM - Cinema Lab CinemaRepository updated

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -37,21 +37,21 @@ public interface CinemaRepository extends JpaRepository<Cinema,Long> {
 
     // ------------------- Native QUERIES ------------------- //
 
-//    //Write a native query to read all cinemas by location country
-//    @Query(value = "SELECT * FROM cinema c JOIN location l on l.id=c.location_id WHERE l.country=?1",nativeQuery = true)
-//    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
-//
-//    //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
-//    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%',?1,'%') OR sponsored_name ILIKE concat('%',?1,'%')",nativeQuery = true )
-//    List<Cinema> retrieveAllByNameOrSponsoredName(String pattern);
-//
-//    //Write a native query to sort all cinemas by name
-//    @Query(value = "SELECT * FROM cinema ORDER BY name",nativeQuery = true)
-//    List<Cinema> sortByName();
-//
-//    //Write a native query to distinct all cinemas by sponsored name
-//    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema",nativeQuery = true)
-//    List<String> distinctBYSponsoredName();
+    //Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c JOIN location l on l.id=c.location_id WHERE l.country=?1",nativeQuery = true)
+    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
+
+    //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%',?1,'%') OR sponsored_name ILIKE concat('%',?1,'%')",nativeQuery = true )
+    List<Cinema> retrieveAllByNameOrSponsoredName(String pattern);
+
+    //Write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name",nativeQuery = true)
+    List<Cinema> sortByName();
+
+    //Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema",nativeQuery = true)
+    List<String> distinctBYSponsoredName();
 
 
 
