eca44fb045a231e5383dd6302c9fdb94d23d977e
SamKar <letmedevelopsam@gmail.com>
2021-05-30 14:12:24 +0300

    Spring - 09 - ORM - Cinema Lab CinemaRepository updated

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -11,24 +11,24 @@ import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema,Long> {
-//
-//    // ------------------- DERIVED QUERIES ------------------- //
-//
-//
-//
-//    //Write a derived query to get cinema with a specific name
-//    Optional<Cinema> findByName(String name);
-//
-//    //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-//    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredNameAsc(String sponsoredname);
-//
-//    //Write a derived query to list all cinemas in a specific country
-//    List<Cinema> findAllByLocationCountry(String country);
-//
-//    //Write a derived query to list all cinemas with a specific name or sponsored name
-//    List<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredname);
-//
-//
+
+    // ------------------- DERIVED QUERIES ------------------- //
+
+
+
+    //Write a derived query to get cinema with a specific name
+    Optional<Cinema> findByName(String name);
+
+    //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
+    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredNameAsc(String sponsoredname);
+
+    //Write a derived query to list all cinemas in a specific country
+    List<Cinema> findAllByLocationCountry(String country);
+
+    //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredname);
+
+
 //    // ------------------- JPQL QUERIES ------------------- //
 //
 //    //Write a JPQL query to read the cinema name with a specific id
