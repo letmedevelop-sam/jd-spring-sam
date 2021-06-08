d0e7d0d5e7c1a2473453914886cc2b4d992f5e95
SamKar <letmedevelopsam@gmail.com>
2021-06-08 06:53:37 +0300

    Spring-11-API-REST-HttpVerbs-Legacy  created

---
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/entity/Product.java
@@ -0,0 +1,27 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+}
