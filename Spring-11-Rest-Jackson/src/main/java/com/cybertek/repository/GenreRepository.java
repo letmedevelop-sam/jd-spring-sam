469e854e2839b75ac1a079bdf4579f49682d34ea
SamKar <letmedevelopsam@gmail.com>
2021-06-09 09:43:10 +0300

    Spring-11-API-REST-Jackson queries commented-out and HomeController added

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/GenreRepository.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -9,14 +9,14 @@ import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
-
-    // ------------------- JPQL QUERIES ------------------- //
-
-    //Write a JPQL query that return all genres
-    @Query("SELECT g FROM Genre g")
-    List<Genre> fetchAllJPQL();
-
-    // ------------------- Native QUERIES ------------------- //
+//
+//    // ------------------- JPQL QUERIES ------------------- //
+//
+//    //Write a JPQL query that return all genres
+//    @Query("SELECT g FROM Genre g")
+//    List<Genre> fetchAllJPQL();
+//
+//    // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
     @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
