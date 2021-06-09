3b7f993ab9e3171e7d80591e41a50bcbb0945032
SamKar <letmedevelopsam@gmail.com>
2021-06-09 06:05:36 +0300

    Spring-11-API-REST-HttpVerbs-Preferred fixed

--- Spring-11-Rest-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-Rest-HttpVerbs-Preferred/src/main/java/com/cybertek/controller/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+//@Controller       //We will use @RestController   and remove all   @ResponseBody
+@RestController
+@RequestMapping("/products") //we defined in application.properties now we need to ad /api to our URI while we test in Postman http://localhost:8080/api/products
 public class ProductController {
 
     private ProductService productService;
@@ -16,33 +18,32 @@ public class ProductController {
         this.productService = productService;
     }
 
-        //GET
-    @RequestMapping(value = "/products/{id}")       //This is creating end point
-    public @ResponseBody                        //As default it is GET ->@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
-    Product getProduct(@PathVariable("id") long id){
+        //GET           //We replaced @RequestMapping with    @GetMapping
+    @GetMapping(value = "/{id}")       //We will not need products "/products/{id}"
+                            //As default it is GET ->@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    public Product getProduct(@PathVariable("id") long id){
         return  productService.getProduct(id);
     }
 
-    @RequestMapping(value = "/products")
-    public @ResponseBody
-    List<Product> getProducts(){
-
+    //bring all
+    @GetMapping      //We replaced @RequestMapping with    @GetMapping        //we deleted (value = "/products")
+    public List<Product> getProducts(){
         return productService.getProducts();
     }
 
-        //POST
-    @RequestMapping(value = "/products",method = RequestMethod.POST)
-    public @ResponseBody List<Product> createProduct(@RequestBody Product product){
+        //POST          //We replaced @RequestMapping with    @PostMapping
+    @PostMapping        // we deleted  (method = RequestMethod.POST)
+    public  List<Product> createProduct(@RequestBody Product product){      // we deleted @ResponseBody
         return productService.createProduct(product);
     }
-        //DELETE
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.DELETE)
-    public @ResponseBody List<Product> deleteProduct(@PathVariable("id") long id){
+        //DELETE    //We replaced @RequestMapping with    @DeleteMapping
+    @DeleteMapping(value = "/{id}")      //we deleted (value = "/products") and ,method = RequestMethod.DELETE
+    public List<Product> deleteProduct(@PathVariable("id") long id){        // we deleted @ResponseBody
         return productService.delete(id);
     }
-        //PUT
-    @RequestMapping(value = "/products/{id}",method = RequestMethod.PUT)
-    public @ResponseBody List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){
+        //PUT            //We replaced @RequestMapping with    @PutMapping
+    @PutMapping(value = "/{id}")        //we deleted (value = "/products") and ,method = RequestMethod.PUT
+    public  List<Product> updateProduct(@PathVariable("id") long id,@RequestBody Product product){       // we deleted @ResponseBody
         return productService.updateProduct(id, product);
     }
 
