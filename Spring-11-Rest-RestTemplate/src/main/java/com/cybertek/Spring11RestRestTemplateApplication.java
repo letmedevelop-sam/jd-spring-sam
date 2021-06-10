b5b4156113aebc9068912cc8ae394768259b2c41
SamKar <letmedevelopsam@gmail.com>
2021-06-10 06:16:01 +0300

    Spring-11-API-REST-RestTemplate test1

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/Spring11RestRestTemplateApplication.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/Spring11RestRestTemplateApplication.java
@@ -2,6 +2,11 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class Spring11RestRestTemplateApplication {
@@ -10,4 +15,11 @@ public class Spring11RestRestTemplateApplication {
         SpringApplication.run(Spring11RestRestTemplateApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .setReadTimeout(Duration.ofMillis(3000))
+                .build();
+    }
 }
