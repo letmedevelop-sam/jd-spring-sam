d9c1c7d85c2991e49ee6c073f9d71bfcaee2a569
SamKar <letmedevelopsam@gmail.com>
2021-06-17 07:57:29 +0300

    Spring-11-API-REST-OpenApi 3.0 user and cinema

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/Application.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/Application.java
@@ -1,7 +1,10 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @Bean
+    public OpenAPI customOpenApi(){
 
+        return new OpenAPI()
+                .info(new Info().title("Cinema Application")
+                        .version("v1")
+                        .description("Selling online cinema tickets"));
+    }
 }
--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/repository/GenreRepository.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -10,17 +10,17 @@ import java.util.List;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
 
-    // ------------------- JPQL QUERIES ------------------- //
-
-    //Write a JPQL query that return all genres
-    @Query("SELECT g FROM Genre g")
-    List<Genre> fetchAllJPQL();
-
-    // ------------------- Native QUERIES ------------------- //
-
-    //Write a native query that returns genres by containing name
-    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
-    List<Genre> retrieveByName(String name);
+//    // ------------------- JPQL QUERIES ------------------- //
+//
+//    //Write a JPQL query that return all genres
+//    @Query("SELECT g FROM Genre g")
+//    List<Genre> fetchAllJPQL();
+//
+//    // ------------------- Native QUERIES ------------------- //
+//
+//    //Write a native query that returns genres by containing name
+//    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')",nativeQuery = true)
+//    List<Genre> retrieveByName(String name);
 
 
 }
