754ea0b3c553c68595a8e6c5dcc3afc191af4823
SamKar <letmedevelopsam@gmail.com>
2021-06-19 17:34:42 +0300

    Spring-11-API-REST-Security - JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/Spring11RestSecurityJwtApplication.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/Spring11RestSecurityJwtApplication.java
@@ -2,6 +2,8 @@ package com.cybertek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Spring11RestSecurityJwtApplication {
@@ -10,4 +12,8 @@ public class Spring11RestSecurityJwtApplication {
         SpringApplication.run(Spring11RestSecurityJwtApplication.class, args);
     }
 
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
 }
